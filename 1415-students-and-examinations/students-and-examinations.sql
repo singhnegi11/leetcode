46b2fcbcacd850a1145112224b6392d64b1a0078
Himanshu Singh Negi <singhnegi11@users.noreply.github.com>
2026-05-15 10:09:15 +0530

    Time: 472 ms (30.43%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,10 +1,17 @@
 /* Write your T-SQL query statement below */
-with cte as (select * from students st 
-cross join subjects sb)
-select ct.student_id,ct.student_name,ct.subject_name,count(et.subject_name) as attended_exams  from cte ct
-left join examinations et 
-on ct.student_id = et.student_id and ct.subject_name = et.subject_name
+--with cte as (select * from students st 
+--cross join subjects sb)
+--select ct.student_id,ct.student_name,ct.subject_name,count(et.subject_name) as attended_exams  from cte ct
+--left join examinations et 
+--on ct.student_id = et.student_id and ct.subject_name = et.subject_name
 --order by ct.student_id
-group by ct.student_id,ct.student_name,ct.subject_name
+--group by ct.student_id,ct.student_name,ct.subject_name
+
+select st.student_id,st.student_name,sb.subject_name,count(et.subject_name) as attended_exams  from students st 
+cross join subjects sb
+left join examinations et 
+on st.student_id = et.student_id and sb.subject_name = et.subject_name
+--order by ct.student_id
+group by st.student_id,st.student_name,sb.subject_name
 
 
